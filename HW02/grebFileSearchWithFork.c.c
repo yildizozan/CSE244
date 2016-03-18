c0ec36b4ce3661fd23534651c9f648b984f85779
Ozan Yıldız <social@yildizozan.com>
2016-03-18 17:27:25 +0200

    Include waitpid() function.

--- HW02/grebFileSearchWithFork.c.c
+++ HW02/grebFileSearchWithFork.c.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[])
 {
@@ -27,24 +28,33 @@ int main(int argc, char *argv[])
 		// Sırayla dosyları okuyacak.
 		while ((ent = readdir(dir)) != NULL)
 		{
-			pid_t childpid = fork();
+			pid_t childPid = fork();
 
-			if (childpid < 0) // Child process oluşmaz ise çıkış yapacak o process
+			if (childPid < 0) // Child process oluşmaz ise çıkış yapacak o process
 			{
 				perror("no child\n");
-				exit (EXIT_FAILURE);
+				exit(1);
 			}
-			else if(childpid == 0) // Başarılı olan processler işleme girecek.
+			else if(childPid == 0) // Başarılı olan processler işleme girecek.
 			{
-				printf("alt process\n");
-				//printf("%s\n", ent->d_name);
-				return 1;
+				printf("%s -> \tpid: %d -> ", ent->d_name, getpid());
+				exit(0);
 			}
-			else
-			{
-				int returnStatus;
-				wait(NULL);
-			}
+			else  // Main (parent) process after fork succeeds 
+			{    
+			    int returnStatus;    
+			    waitpid(childPid, &returnStatus, 0);  // Parent process waits here for child to terminate.
+
+			    if (returnStatus == 0)  // Verify child process terminated without error.  
+			    {
+			       printf("The child process terminated normally.\n"); 
+			    }
+
+			    if (returnStatus == 1)      
+			    {
+			       printf("The child process terminated with an error!.\n");   
+			    }
+			} // end else
 			
 		} // end while readdir
 
