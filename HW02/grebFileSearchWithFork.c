2e30060518350c628cb734f679f74adfac10f0a3
Ozan Yıldız <developer@yildizozan.com>
2016-03-19 21:50:26 +0200

    File name changed

---
+++ HW02/grebFileSearchWithFork.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+	// Using:
+	if (argc != 3)
+	{
+		printf("Using: ./grD [Directory Name] text\n");
+		return 1;
+	}
+
+	// Folder variables
+	DIR *dir;
+	struct dirent *ent;
+	
+	// Try to open folder
+	if ((dir = opendir(argv[1])) == NULL) // Argüman olarak değişecek
+	{
+		perror("opendir");
+		return 1;
+	}
+	else // Dosya başarıyla açılırsa buradan devam edecek.
+	{
+		// Sırayla dosyları okuyacak.
+		while ((ent = readdir(dir)) != NULL)
+		{
+			pid_t childPid = fork();
+
+			if (childPid < 0) // Child process oluşmaz ise çıkış yapacak o process
+			{
+				perror("no child\n");
+				exit (EXIT_FAILURE);
+			}
+			else if(childPid == 0) // Başarılı olan processler işleme girecek.
+			{
+				printf("alt process\n");
+				//printf("%s\n", ent->d_name);
+				return 1;
+			}
+			else
+			{
+				int returnStatus;
+				wait(NULL);
+			}
+			
+		} // end while readdir
+
+	} // end else
+
+	closedir(dir);
+		
+	return 0;
+}
--- HW02/grebFileSearchWithFork.c.c
+++ HW02/grebFileSearchWithFork.c.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <dirent.h>
-#include <unistd.h>
-
-int main(int argc, char *argv[])
-{
-	// Using:
-	if (argc != 3)
-	{
-		printf("Using: ./grD [Directory Name] text\n");
-		return 1;
-	}
-
-	// Folder variables
-	DIR *dir;
-	struct dirent *ent;
-	
-	// Try to open folder
-	if ((dir = opendir(argv[1])) == NULL) // Argüman olarak değişecek
-	{
-		perror("opendir");
-		return 1;
-	}
-	else // Dosya başarıyla açılırsa buradan devam edecek.
-	{
-		// Sırayla dosyları okuyacak.
-		while ((ent = readdir(dir)) != NULL)
-		{
-			pid_t childpid = fork();
-
-			if (childpid < 0) // Child process oluşmaz ise çıkış yapacak o process
-			{
-				perror("no child\n");
-				exit (EXIT_FAILURE);
-			}
-			else if(childpid == 0) // Başarılı olan processler işleme girecek.
-			{
-				printf("alt process\n");
-				//printf("%s\n", ent->d_name);
-				return 1;
-			}
-			else
-			{
-				int returnStatus;
-				wait(NULL);
-			}
-			
-		} // end while readdir
-
-	} // end else
-
-	closedir(dir);
-		
-	return 0;
-}
+  #include <dirent.h>		  #include <dirent.h>
+  #include <unistd.h>		  #include <unistd.h>
+ +#include <sys/wait.h>
+  		  
+  int main(int argc, char *argv[])		  int main(int argc, char *argv[])
+  {		  {
+ @@ -27,24 +28,33 @@ int main(int argc, char *argv[])
+  		// Sırayla dosyları okuyacak.		  		// Sırayla dosyları okuyacak.
+  		while ((ent = readdir(dir)) != NULL)		  		while ((ent = readdir(dir)) != NULL)
+  		{		  		{
+ -			pid_t childpid = fork();		 +			pid_t childPid = fork();
+  		  
+ -			if (childpid < 0) // Child process oluşmaz ise çıkış yapacak o process		 +			if (childPid < 0) // Child process oluşmaz ise çıkış yapacak o process
+  			{		  			{
+  				perror("no child\n");		  				perror("no child\n");
+ -				exit (EXIT_FAILURE);		 +				exit(1);
+  			}		  			}
+ -			else if(childpid == 0) // Başarılı olan processler işleme girecek.		 +			else if(childPid == 0) // Başarılı olan processler işleme girecek.
+  			{		  			{
+ -				printf("alt process\n");		 +				printf("%s -> \tpid: %d -> ", ent->d_name, getpid());
+ -				//printf("%s\n", ent->d_name);		 +				exit(0);
+ -				return 1;		
+ -			}		
+ -			else		
+ -			{		
+ -				int returnStatus;		
+ -				wait(NULL);		
+  			}		  			}
+ +			else  // Main (parent) process after fork succeeds 
+ +			{    
+ +			    int returnStatus;    
+ +			    waitpid(childPid, &returnStatus, 0);  // Parent process waits here for child to terminate.
+ +
+ +			    if (returnStatus == 0)  // Verify child process terminated without error.  
+ +			    {
+ +			       printf("The child process terminated normally.\n");       
+ +			    }
+ +
+ +			    if (returnStatus == 1)      
+ +			    {
+ +			       printf("The child process terminated with an error!.\n");   
+ +			    }
+ +			} // end else
+  					  			
+  		} // end while readdir
+ } // end while readdir
