4c9d2bd42f8559384b787675d52c9c5c6dbe8354
Ozan Yıldız <developer@yildizozan.com>
2016-03-31 13:48:02 +0300

    Include pipe
    It's working!

--- HW03/grepFileDirWithPipe.c
+++ HW03/grepFileDirWithPipe.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
 
-#define BUFFER_SIZE 512
+#define BUFFER_SIZE 1
 
 void fileCheck(const char *path, const char *text);
 
 // Pipe functions
-void pipeWriting(int fd, const char *path);
-void pipeReading(int fd);
+void pipeWriting(const int fd, const char *path);
+void pipeReading(const int fd);
 
 int main(int argc, char const *argv[])
 {
@@ -27,7 +29,6 @@ int main(int argc, char const *argv[])
 
 void fileCheck(const char *path, const char *text)
 {
-
 	int fileDescription[2];
 
 	if (pipe(fileDescription) < 0)
@@ -47,6 +48,8 @@ void fileCheck(const char *path, const char *text)
 		}
 		else if (pid == 0) // Child proces
 		{
+
+
 			close(fileDescription[0]);
 			pipeWriting(fileDescription[1], path);
 			close(fileDescription[1]);
@@ -62,28 +65,59 @@ void fileCheck(const char *path, const char *text)
 	}
 }
 
-void pipeWriting(int fd, const char *path)
+//
+//   FUNCTION: pipeWriting(const int, const char *)
+//
+//   PURPOSE: Writing with pipe
+//
+//   COMMENTS:
+//
+//		Aldığı path argümanı ile ilgili dosyayı READONLY modunda açar.
+//		Daha sonra açmış olduğu bu dosyadan BUFFER_SIZE miktarı kadar okur.
+//		BUFFER_SIZE miktarı default olarak 1 olarak ayarladım.
+//		Her bir byte pipe dosyasına yazılır ve dosya kapatılır.
+//
+void pipeWriting(const int fd, const char *path)
 {
 	char bufferSize[BUFFER_SIZE];
 	ssize_t n;
+	int fdf;
 
-	while((n = read(fd, bufferSize, BUFFER_SIZE)) > 0)
-		if (write(STDOUT_FILENO, bufferSize, n) != n)
+	if ((fdf = open(path, O_RDONLY)) < 0)
+		perror("open");
+
+	while((n = read(fdf, bufferSize, BUFFER_SIZE)) > 0)
+		if (write(fd, bufferSize, n) != n)
 			perror("write");
 
 	if (n < 0)
 		perror("read");
+
+	close(fdf);
+	
+	return;
 }
 
-void pipeReading(int fd)
+//
+//   FUNCTION: pipeReading(const int)
+//
+//   PURPOSE: Reading with pipe
+//
+//   COMMENTS:
+//
+//		Dosyadan okumayı sağlar pipe ile 
+//
+void pipeReading(const int fd)
 {
 	char bufferSize[BUFFER_SIZE];
 	ssize_t n;
 
 	while((n  = read(fd, bufferSize, BUFFER_SIZE)) > 0)
-		if (write(fd, bufferSize, n) < 0)
+		if (write(STDOUT_FILENO, bufferSize, n) < 0)
 			perror("pipe");
 
 	if (n < 0)
 		perror("read");
+
+	return;
 }
