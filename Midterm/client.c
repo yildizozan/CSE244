3a59f925e8ca1439be5ac3eca80cf2305fc24acc
Ozan Yıldız <developer@yildizozan.com>
2016-04-17 04:09:05 +0300

    Connection fifo working but there is tiny error.

--- Midterm/client.c
+++ Midterm/client.c
@@ -1,39 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <errno.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 
 #define BUFFER_SIZE 4096
 
 int main()
 {
-	/* Fifo */
+	/*
+	Fifo variables
+	*/
 	int fifoFileDescription;
 	char fifoBuffer[BUFFER_SIZE];
+
+	/* Control printf */
+	printf("-%d\n", (int)getpid());
+
 	/*
-		We can try to open fifo file for server connection
+	We can try to open fifo file for server connection
 	*/
-	if ((fifoFileDescription = open("Connection", O_WRONLY)) < 0)
+	if ((fifoFileDescription = open("Connection", O_RDWR)) < 0)
 	{
 		printf("Server not open\nExiting..");
 		exit(EXIT_FAILURE);
 	}
 
-	printf("Your message to server: ");
-	scanf("%s", fifoBuffer);
-
 	/*
-		*
+	Client send pid number to server.
+	Server create new fifo with client pid number.
 	*/
-	if (write(fifoFileDescription, fifoBuffer, BUFFER_SIZE) < 0)
+	sprintf(fifoBuffer, "%d", (int)getpid());
+	if (write(fifoFileDescription, fifoBuffer, sizeof(fifoBuffer)) < 0)
 	{
-		printf("Fifo gönderilemedi..\n");
+		printf("Not connecting..\n");
+		exit(EXIT_FAILURE);
 	}
 
+	if (read(fifoFileDescription, fifoBuffer, sizeof(fifoBuffer)) < 0)
+	{
+		printf("Server busy, error code: 659..\nExiting..\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Control printf */
+	printf("--%s\n", fifoBuffer);
+
+	/*
+	Server OK message
+	*/
+	if (strcmp(fifoBuffer, "OK!") != 0)
+	{
+		printf("Yannis\n");
+		exit(EXIT_FAILURE);
+	}
+
+	/* Close connection fifo */
 	close(fifoFileDescription);
+
 
 
 	return 1;
--- Midterm/server.c
+++ Midterm/server.c
@@ -14,9 +14,9 @@ int main(int argc, char *argv[])
 	/*
 	if (argc != 3)
 	{
-		printf("Wrong parameters!\n");
-		printf("Usage: ./server <(int)resulution> <(int)maxClient>\n");
-		exit(EXIT_FAILURE);
+	printf("Wrong parameters!\n");
+	printf("Usage: ./server <(int)resulution> <(int)maxClient>\n");
+	exit(EXIT_FAILURE);
 	}
 	*/
 
@@ -31,37 +31,55 @@ int main(int argc, char *argv[])
 	printf("%f - %d\n", resulution, maxClient);
 
 	/*
-		We'll try to create fifo file for connection
-		if there is a fifo connection file, exiting for security
+	We'll try to create fifo file for connection
+	if there is a fifo connection file, exiting for security
 	*/
-	if (mkfifo("Connection", 0666) < 0)
+	if (mkfifo("Connection", 0666) < 0 && (errno != EEXIST))
 	{
 		printf("Cannot create 'Connection' fifo file..\n");
 		printf("We'll exiting..\n");
 		exit(EXIT_FAILURE);
 	}
 
-	if ((fifoFileDescripton = open("Connection", O_RDONLY)) < 0)
+	if ((fifoFileDescripton = open("Connection", O_RDWR)) < 0)
 	{
 		printf("Cannot open fifo file, please try again\n");
 		printf("We'll exiting..\n");
 		exit(EXIT_FAILURE);
 	}
 	else
-		printf("Server working and waiting client..\n");
+		printf("Server working and waiting client(s)..\n");
 
-	while(1)
-	{
-		if (0 < read(fifoFileDescripton, fifoBuffer, BUFFER_SIZE))
+	/*
+	Waiting clients
+	*/
+	while (1)
+	{	/* If success */
+		read(fifoFileDescripton, fifoBuffer, sizeof(fifoBuffer));
+
+		/*
+		Close server commant
+		*/
+		if (strcmp(fifoBuffer, "exit") == 0)
 		{
-			printf("Client message: %s\n", fifoBuffer);
-		}/* end if read */
+			printf("U r admin!\nShut down server..\n");
+			exit(EXIT_SUCCESS);
+		}
+
+		printf("Client pid: %s connecting..\n", fifoBuffer);
+
+		strcpy(fifoBuffer, "OK!");
+		write(fifoFileDescripton, fifoBuffer, sizeof(fifoBuffer));
 
 
-      memset(fifoBuffer, 0, BUFFER_SIZE);
+
+		memset(fifoBuffer, 0, BUFFER_SIZE);
 
 	} /* end while */
 
+	  /*
+	  Close and delete fifo files
+	  */
 	close(fifoFileDescripton);
 	unlink("Connection");
 
