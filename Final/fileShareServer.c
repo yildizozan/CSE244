79d2c7b122e162b66003243f57f903b468905890
Ozan Yıldız <developer@yildizozan.com>
2016-05-22 01:41:03 +0300

    Update fileShareServer.c
    Server OK!

--- Final/fileShareServer.c
+++ Final/fileShareServer.c
@@ -1,10 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void communication(int);
 
 int main(int argc, char const *argv[])
 {
-    
+    /* General variables */
+    int quit = 1;
+
+    /* Fork variables */
+    int childPid;
+
+    /* Socket variables */
+    int port;
+    int socketFD;
+    int socketAcceptFD;
+    struct sockaddr_in serverAddr;
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLengt;
 
     if (argc != 2)
     {
@@ -12,7 +31,86 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE);
     }
 
-    printf("-> %s\n", argv[1]);
+    /* Creating socket */
+    socketFD = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketFD < 0)
+    {
+        perror("Error 213");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Initialize socket */
+    bzero((char *) &serverAddr, sizeof(serverAddr));
+
+    port = atoi(argv[1]);
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(port);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    /* Socket Bind */
+    if (bind(socketFD, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
+    {
+        perror("Error 214");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Starting to liston for the clientsi now! */
+    listen(socketFD, 1);
+
+    clientAddrLengt = sizeof(clientAddr);
+
+    while(quit)
+    {
+        /* Socket waiting */
+        socketAcceptFD = accept(socketFD, (struct sockaddr *) &clientAddr, &clientAddrLengt);
+        if (socketAcceptFD < 0)
+        {
+            perror("Error 332");
+        }
+
+        /* Create child process */
+        childPid = fork();
+        if (childPid < 0)
+        {
+            perror("Error 334");
+            exit(1);
+        }
+
+        /* This is the client process */
+        if (childPid == 0)
+        {
+            close(socketFD);
+            communication(socketAcceptFD);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            close(socketAcceptFD);
+        }
+    }
+
+    close(socketFD);
 
     return 0;
+}
+
+void communication(int newSocket)
+{
+    int n;
+    char buffer[256];
+    bzero(buffer,256);
+
+    n = read(newSocket,buffer,255);
+    if (n < 0) {
+        perror("ERROR reading from socket");
+        exit(1);
+    }
+
+    printf("Here is the message: %s\n",buffer);
+
+    n = write(newSocket,"I got your message",18);
+    if (n < 0) {
+        perror("ERROR writing to socket");
+        exit(1);
+    }
 }
